0c23acf924ebd5c1398ae36cf656fe756edcd672
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-06-15 20:02:31 +0700

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,30 +8,39 @@ class HomeController < ApplicationController
   end
 
   def create
-    
-    # unless File.exist?("Demo.json")
-      # output = File.open("Demo.json","w")
-    if input_params[:file].present?
-      begin
-        file = input_params[:file]
-        output = JSON.pretty_generate(CSV.open(file.path, :headers => true).map { |x| x.to_h}) 
-      rescue
-        output = "invalid input"
+    @output = ""
+    @json = ""
+    csv_str = input_params[:input]
+    CSV.parse(csv_str) do |row| 
+      # puts " #{row[0]}#{row[1]}#{row[2]} #{row[3]}#{row[4]}" 
+      unless row.empty?
+        unless row[0].nil?
+          @post = row[0]
+          @api = row[1]
+          if @json.empty?
+            @json += "\n{\n  \"#{row[3]}\" : \"#{row[4]}\""
+          end
+          else
+            @json +=  ",\n  \"#{row[3]}\" : \"#{row[4]}\""
+          end   
+      else
+        @json += "\n}"
+        @output += "METHOD:   " + @post + "\n" + "API_URL:   " + @api + "\n" + "JSON :" + @json + "\n\n"
+        @json = ""
       end
-    end
-        
-      # else  
-        # output = ''
-      # output.close
+    end 
+   @output
+
+    # if input_params[:file].present?
+    #   begin
+    #     file = input_params[:file]
+    #     output = JSON.pretty_generate(CSV.open(file.path, :headers => true).map { |x| x.to_h}) 
+    #   rescue
+    #     output = "invalid input"
+    #   end
     # end
 
-    # file = File.open("Demo.json")
-    # @output = file.read
-    # file.close
-    # p @output
-    @output = output
     render 'new'
-    # File.delete(file)
   end
 
   private
